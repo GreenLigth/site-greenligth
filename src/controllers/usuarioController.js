4a4fe3536456ef8f974cf24afdd1a90c876a3fe1
MaxAraujo13 <mariana.silva@sptech.school>
2026-05-10 15:41:35 -0300

    Incialização da inserção de dados de cadastro de novos usuários a partir da tela de administrador - Max Araujo

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -76,7 +76,51 @@ function cadastrar(req, res) {
     }
 }
 
+//Cadastro Usuario 
+
+
+function cadastrarUsuario(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastroUsuario.html
+    var nome = req.body.nome;
+    var email = req.body.email;
+    var senha = req.body.senha;
+    var cargo = req.body.cargo;
+    var nomeEstufa = req.body.nomeEstufa;
+
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else if (cargo == undefined) {
+        res.status(400).send("Seu cargo está undefined!");
+    } else if (nomeEstufa == undefined) {
+        res.status(400).send("O nome da sua estufa está undefined");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrarUsuario(nome, cargo, email, senha, nomeEstufa)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar, 
+    cadastrarUsuario
 }
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -31,7 +31,39 @@ function cadastrar(nome,cnpj, email, senha, nomeEstufa) {
     console.log("Executando a instrução SQL: \n" + instrucaoEmpresa + instrucaoEstufa);
 }
 
+// Cadastro de usuario na dashboard
+
+function cadastrarUsuario(nome,cargo, email, senha, nomeEstufa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email,cargo, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoEmpresa = `
+        INSERT INTO empresa (nome,cargo, email, senha) VALUES ('${nome}','${cargo}', '${email}', '${senha}');
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoEmpresa);
+
+    return database.executar(instrucaoEmpresa)
+        .then(function(resultadoEmpresa){
+            var idEmpresa = resultadoEmpresa.insertId;
+            
+            var instrucaoEstufa = `
+            INSERT INTO estufa (nomeEstufa, fkEmpresa) VALUES ('${nomeEstufa}', ${idEmpresa})
+            `;
+
+            console.log("Executando a instrução SQL: \n" + instrucaoEstufa);
+            
+             return database.executar(instrucaoEstufa);
+
+
+
+        })
+
+}
+
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    cadastrarUsuario
 };
