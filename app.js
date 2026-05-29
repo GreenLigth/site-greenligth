4a93fee561cd98289ce83a88b99aeab6d6c15b75
juliasilva0957 <julia.silva@sptech.school>
2026-05-29 11:07:22 -0300

    Subindo alterações no SRC

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ var HOST_APP = process.env.APP_HOST;
 
 var app = express();
 
-var indexRouter = require("./src/routes/index");
-var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
 var registrosRouter = require("./src/routes/registros");
 var usuarioDashRouter = require("./src/routes/usuarioDash");
